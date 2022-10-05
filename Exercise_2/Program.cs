d62ccb889d81b12f6120cf9bee7919dc84d0a7c9
StasyanSV <stanislav_xx@mail.ru>
2022-10-06 00:39:21 +0400

    add Exercise_1 and Exercise_2 and Exercise_3

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите любое целое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+if (number_1 > 99 || number_1 < -99)
+{
+    number_1 = Math.Abs(number_1);
+
+    //Подсчет колличества цифр в введенном числе
+    int x = number_1, counter = 0;
+    while (x > 0)
+    {
+        x = x / 10;
+        counter++;
+    }
+    //Console.WriteLine($"Цифр в числе: {counter}");
+
+    //Деление на 10 в степени "{counter}-3". Деление числа до третьей цифры
+    int number_2 = number_1;
+    while (counter-3 > 0)
+    {
+        number_2 = number_2 / 10;
+        counter--;
+    }
+    //Console.WriteLine($"Полученное число: {number_2}");
+
+    //Вычисление третьей цифры
+    number_2 = number_2 % 10;
+
+    Console.Write($"Введенное число: {number_1} -> третья цифра: {number_2} ");
+}
+else
+{
+    Console.Write($"Введенное число: {number_1} -> третьей цифра: нет.");
+}
---
+++ Exercise_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите число от 1 до 7, где число обозначает день недели соотвественно: ");
+int number_1 = int.Parse(Console.ReadLine());
+
+switch (number_1)
+{
+    case 1:
+        Console.WriteLine($"Вы ввели число {number_1} - Понедельник -> рабочий день.");
+        break;
+    case 2:
+        Console.WriteLine($"Вы ввели число {number_1} - Вторник -> рабочий день.");
+        break;
+    case 3:
+        Console.WriteLine($"Вы ввели число {number_1} - Среда -> рабочий день.");
+        break;
+    case 4:
+        Console.WriteLine($"Вы ввели число {number_1} - Четверг -> рабочий день.");
+        break;
+    case 5:
+        Console.WriteLine($"Вы ввели число {number_1} - Пятница -> рабочий день.");
+        break;
+    case 6:
+        Console.WriteLine($"Вы ввели число {number_1} - Суббота -> выходной день.");
+        break;
+    case 7:
+        Console.WriteLine($"Вы ввели число {number_1} - Суббота -> выходной день.");
+        break;
+    default:
+        Console.WriteLine($"Вы ввели число {number_1} не соответсвующее условию задачи.");
+        break;
+}
